452ed21ed62e87184ca18c233607e8e0f2d4d114
Elvis <elvis.e.amancio@gmail.com>
2021-04-16 13:35:06 -0300

    Responsividade no Chakra

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Logo() {
   return (
-    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+    <Text fontSize={["2xl", "3xl"]} fontWeight="bold" letterSpacing="tight" w="64">
       DashGo<Text as="span" ml="1" color="pink.500">.</Text>
     </Text>
   )
